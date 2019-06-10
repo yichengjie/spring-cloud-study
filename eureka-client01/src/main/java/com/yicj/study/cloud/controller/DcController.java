f409cf9239fb56eb82c13720b8c383c7592b8326
yicj <626659321@qq.com>
2019-06-10 21:35:09 +0800

    ribbon消费端注册

--- eureka-client01/src/main/java/com/yicj/study/cloud/controller/DcController.java
+++ eureka-client01/src/main/java/com/yicj/study/cloud/controller/DcController.java
@@ -1,11 +1,13 @@
 package com.yicj.study.cloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class DcController {
 
 	@Autowired
@@ -14,7 +16,7 @@ public class DcController {
     @GetMapping("/dc")
     public String dc() {
         String services = "Services: " + discoveryClient.getServices();
-        System.out.println(services);
+        log.info("provider service ======> " + services);
         return services;
     }
 }
---
+++ eureka-consumer-ribbon/src/main/java/com/yicj/study/cloud/controller/DcController.java
@@ -0,0 +1,22 @@
+package com.yicj.study.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class DcController {
+
+    @Autowired
+    private RestTemplate restTemplate ;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return restTemplate.getForObject("http://eureka-client01/dc", String.class);
+    }
+}
