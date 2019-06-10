bdfc3d701f9671800dd6eb8943dbee1c3131371a
yicj <626659321@qq.com>
2019-06-10 21:13:45 +0800

    消费端

---
+++ eureka-consumer01/src/main/java/com/yicj/study/cloud/Application.java
@@ -0,0 +1,25 @@
+package com.yicj.study.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+//https://blog.csdn.net/u011531425/article/details/81713441
+//http://blog.didispace.com/spring-cloud-starter-dalston-1/
+//Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+            .web(true).run(args);
+    }
+
+    //初始化RestTemplate，用来真正发起REST请求
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate() ;
+    }
+}
---
+++ eureka-consumer01/src/main/java/com/yicj/study/cloud/controller/DcController.java
@@ -0,0 +1,33 @@
+package com.yicj.study.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class DcController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient ;
+
+    @Autowired
+    private RestTemplate restTemplate ;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance =
+                this.loadBalancerClient.choose("eureka-client01") ;
+        String url = "http://" + serviceInstance.getHost()+":"
+                + serviceInstance.getPort() +"/dc" ;
+        log.info("url :{}",url);
+        return restTemplate.getForObject(url,String.class) ;
+    }
+}
