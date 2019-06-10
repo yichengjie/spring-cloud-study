0cf543b2aa5fb6266e2de199f636a9c54615041b
yicj <626659321@qq.com>
2019-06-10 22:13:39 +0800

    准备文件上传

--- eureka-client/src/main/java/com/yicj/study/cloud/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/yicj/study/cloud/EurekaClientApplication.java
@@ -3,9 +3,11 @@ package com.yicj.study.cloud;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 //https://blog.csdn.net/u011531425/article/details/81713441
 //http://blog.didispace.com/spring-cloud-starter-dalston-1/
 //Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
