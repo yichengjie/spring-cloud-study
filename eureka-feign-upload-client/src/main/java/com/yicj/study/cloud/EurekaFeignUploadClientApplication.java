8df6ac955879e6edd9a7dcdbc26f09076aeafbea
yicj <626659321@qq.com>
2019-06-11 22:21:24 +0800

    文件上传

---
+++ eureka-feign-upload-client/src/main/java/com/yicj/study/cloud/EurekaFeignUploadClientApplication.java
@@ -0,0 +1,21 @@
+package com.yicj.study.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+
+//https://blog.csdn.net/u011531425/article/details/81713441
+//http://blog.didispace.com/spring-cloud-starter-dalston-1/
+//Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaFeignUploadClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaFeignUploadClientApplication.class)
+            .web(true).run(args);
+    }
+
+}
