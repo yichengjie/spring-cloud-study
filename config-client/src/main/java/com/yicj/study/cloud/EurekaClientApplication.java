2fdaf6b4cefcf04ce7fbf4a0917eb4e4284fda62
yichengjie <626659321@qq.com>
2019-06-12 22:43:48 +0800

    配置中心

--- config-client/src/main/java/com/yicj/study/cloud/EurekaClientApplication.java
+++ config-client/src/main/java/com/yicj/study/cloud/EurekaClientApplication.java
@@ -2,8 +2,7 @@ package com.yicj.study.cloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-//https://blog.csdn.net/u011531425/article/details/81713441
-//http://blog.didispace.com/spring-cloud-starter-dalston-1/
+//https://www.cnblogs.com/hellxz/p/9306507.html
 //Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
 @SpringBootApplication
 public class EurekaClientApplication {
--- config-client/src/main/java/com/yicj/study/cloud/controller/HomeController.java
+++ config-client/src/main/java/com/yicj/study/cloud/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.yicj.study.cloud.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //开启更新功能
+@RequestMapping("api")
 public class HomeController {
+	@Value("${info}")
+	private String info ;
 	
 	@GetMapping("hello")
 	public String hello() {
-		return "hello" ;
+		return info ;
 	}
 }
