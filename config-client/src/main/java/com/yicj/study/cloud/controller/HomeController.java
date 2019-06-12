2abe1e10b4a9d70bc72f3ea4a328718275d64a77
yichengjie <626659321@qq.com>
2019-06-12 22:19:17 +0800

    配置中心demo编写

---
+++ config-client/src/main/java/com/yicj/study/cloud/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.yicj.study.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("hello")
+	public String hello() {
+		return "hello" ;
+	}
+}
---
+++ config-server-git/src/main/java/com/yicj/study/cloud/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.yicj.study.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+//https://blog.csdn.net/u011531425/article/details/81713441
+//http://blog.didispace.com/spring-cloud-starter-dalston-1/
+//Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
+@EnableConfigServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class)
+            .web(true).run(args);
+    }
+}
