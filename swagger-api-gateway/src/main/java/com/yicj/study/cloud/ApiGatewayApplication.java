5c880e336cfb3c1ce4b3c42877d071bf700eb4e0
yichengjie <626659321@qq.com>
2019-06-14 22:01:17 +0800

    暂存

--- swagger-api-gateway/src/main/java/com/yicj/study/cloud/ApiGatewayApplication.java
+++ swagger-api-gateway/src/main/java/com/yicj/study/cloud/ApiGatewayApplication.java
@@ -3,10 +3,6 @@ package com.yicj.study.cloud;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import com.yicj.study.cloud.filter.AccessFilter;
-
 //https://blog.csdn.net/u011531425/article/details/81713441
 //http://blog.didispace.com/spring-cloud-learning/
 //Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
@@ -20,8 +16,4 @@ public class ApiGatewayApplication {
     //http://localhost:1101/eureka-consumer/consumer：返回401错误
     //http://localhost:1101/eureka-consumer/consumer?accessToken=token：
     //正确路由到hello-service的/hello接口，并返回Hello World
-    @Bean
-    public AccessFilter accessFilter() {
-    	return new AccessFilter() ;
-    }
 }
