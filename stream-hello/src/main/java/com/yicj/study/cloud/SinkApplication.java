5189cbcee4e1e90222066d30d17226905a683d39
yichengjie <626659321@qq.com>
2019-06-15 22:38:39 +0800

    暂时提交(stream-hello有报错)

--- stream-hello/src/main/java/com/yicj/study/cloud/SinkApplication.java
+++ stream-hello/src/main/java/com/yicj/study/cloud/SinkApplication.java
@@ -1,5 +1,7 @@
 package com.yicj.study.cloud;
 
+import java.net.URL;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 //https://blog.csdn.net/u011531425/article/details/81713441
@@ -7,8 +9,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 //Eureka Client的属性都在EurekaClientConfig类接口中定义处理方法
 @SpringBootApplication
 public class SinkApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+    	//org.hibernate.validator.internal.engine.ConfigurationImpl
+    	URL resource = SinkApplication.class.getClassLoader().getResource("org/hibernate/validator/internal/engine/ConfigurationImpl.class");
+    	System.out.println(resource);
+    	//.loadClass("org/hibernate/validator/internal/engine/ConfigurationImpl.class").get ;
         new SpringApplicationBuilder(SinkApplication.class)
             .web(true).run(args);
     }
+    
+    
 }
--- stream-hello/src/main/java/com/yicj/study/cloud/service/SinkReceiver.java
+++ stream-hello/src/main/java/com/yicj/study/cloud/service/SinkReceiver.java
@@ -3,7 +3,6 @@ package com.yicj.study.cloud.service;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-
 import lombok.extern.slf4j.Slf4j;
 
 @EnableBinding(Sink.class)
