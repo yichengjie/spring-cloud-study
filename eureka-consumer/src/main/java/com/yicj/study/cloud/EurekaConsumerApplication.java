eed8a1fcadf6cdfa0c214e44865f13eec534ba32
yichengjie <626659321@qq.com>
2019-06-16 12:00:13 +0800

    优化日志

--- eureka-consumer/src/main/java/com/yicj/study/cloud/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/yicj/study/cloud/EurekaConsumerApplication.java
@@ -1,7 +1,7 @@
 package com.yicj.study.cloud;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class EurekaConsumerApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaConsumerApplication.class)
-            .web(true).run(args);
+        SpringApplication.run(EurekaConsumerApplication.class, args) ;
     }
 
     //初始化RestTemplate，用来真正发起REST请求
